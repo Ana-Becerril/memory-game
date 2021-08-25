a1ee64e60e8a64570fed7485414429c700c16525
Ana- Becerril <ana.becbel@gmail.com>
2021-08-25 15:34:18 -0500

    added a card component

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import styles from '../App.module.css'
+import Card from './card/Card'
 
 const Main = () => {
     return (
         <>
           <div className={styles.mainContainer}>
                 <header>Main</header>
+                <Card></Card>
             </div>
         </>
     );
---
+++ src/components/card/Card.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import styles from './Card.module.css'
+
+const Card = () => {
+    return (
+        <>
+          <div className={styles.cardContainer}>
+                <header>Card</header>
+            </div>
+        </>
+    );
+}
+
+export default Card;
