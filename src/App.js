a0ae991294a0f2aac62dacd592a6aaca67b434c7
Ana- Becerril <ana.becbel@gmail.com>
2021-08-27 17:08:25 -0500

    changed matriz and add 3 components

--- src/App.js
+++ src/App.js
@@ -1,22 +1,23 @@
-import React from 'react';
-import styles from './App.module.css';
-import Header from './components/Header'
-import ScoreBoard from './components/ScoreBoard'
-import Main from './components/Main'
+import React, { useState, useEffect } from "react";
+import { useSpring, animated as a } from "react-spring";
+import styles from './App.module.css'
 
-function App() {
+const App = () => {
+  const [highScore, setHighScore] = useState(0)
 
+  useEffect(() => {
+  }, [])
 
-  
   return (
-    <>
-    <div className={styles.titleCounter}>
-    <Header/>
-    <ScoreBoard/>
+    <div>
+      <div className={styles.container}>
+        <h1>Memory Game</h1>
+        <div>High Score: {highScore}</div>
+      </div>
     </div>
-    <Main/>
-    </>
-  );
+    
+  )
 }
 
 export default App;
+
