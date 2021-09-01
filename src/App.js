628547385c81501c3e7fb819827f10bee0f4d731
Ana- Becerril <ana.becbel@gmail.com>
2021-09-01 14:46:22 -0500

    added functions to shuffle the cards

--- src/App.js
+++ src/App.js
@@ -8,34 +8,57 @@ import styles from './App.module.css'
 const App = () => {
   const [score, setScore] = useState(0)
   const [highScore, setHighScore] = useState(0)
+  const [listItems, setListItems] =useState(cards)
 
-  const shuffle = (array) => {
-    for (let i = array.length - 1; i > 0; i--) {
-      const j = Math.floor(Math.random() * (i + 1));
-      [array[i], array[j]] = [array[j], array[i]];
+  function shuffle(arra1) {
+    var ctr = arra1.length,
+      temp,
+      index;
+    while (ctr > 0) {
+      index = Math.floor(Math.random() * ctr);
+      ctr--;
+      temp = arra1[ctr];
+      arra1[ctr] = arra1[index];
+      arra1[index] = temp;
     }
-    return array;
+    return arra1;
   }
+
+  useEffect(() => {
+    setListItems(shuffle(cards))
+  })
+
+ const scoreCounter = () => {
+   setScore(score+1)
+ }
+ if (score > highScore) {
+  setHighScore(score)
+  const json = JSON.stringify(score)
+  localStorage.setItem('memorygamehighscore', json)
+}
+
 
 return (
 <>
+<Wrap>
   <div className={styles.title}>
   <Header/>
   <div className={styles.score}>
-  <h2>High Score:</h2>
-  <h2>Score:</h2>
+  <h2>High Score:{highScore}</h2>
+  <h2>Score:{score}</h2>
   </div>
   </div>
- 
-  <Wrap>
   <div className={styles.cardsContainer}>
-          {cards.map(cards => (
+          {listItems.map(cards => (
             <Card
+              scoreCounter={scoreCounter}
               key={cards.id}
               id={cards.id}
               name={cards.name}
               image={cards.image}
               alt={cards.alt}
+              shuffle={shuffle}
+              cards={cards}
               // selectedImage={selectedImage}
             />
           ))}
--- src/components/Card/index.js
+++ src/components/Card/index.js
@@ -1,10 +1,10 @@
 import React from 'react'
 import styles from './style.module.css'
 
-const Card = ({alt, image, name}) => {
+const Card = ({alt, image, name, scoreCounter, id, shuffle, cards}) => {
   return (
     <div className={styles.card}>
-      {/* <div className="card" onClick={selectedImage(id)}> */}
+      <div className="card" onClick={function(cards){ scoreCounter(); shuffle(cards);}}>
         <div className={styles.imgContainer}>
           <img alt={alt} src={image} />
         </div>
@@ -12,7 +12,7 @@ const Card = ({alt, image, name}) => {
               <div name={name}>{name}</div>
         </div>
       </div>
-    // </div>
+     </div>
   );
 }
 
